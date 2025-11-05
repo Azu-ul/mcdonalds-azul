2b3a8766d08a4bab4b56fcf29f4b04684d4d98a9
Azul Zulu <azulsofiadavid@gmail.com>
2025-11-05 13:59:33 -0300

    cambios en db.js

--- back/db.js
+++ back/db.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'auth_demo',
+  host: process.env.MYSQLHOST || process.env.DB_HOST || '127.0.0.1',
+  user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || '',
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME || 'railway',
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  port: process.env.DB_PORT || 3306
 });
 
 export default pool;
